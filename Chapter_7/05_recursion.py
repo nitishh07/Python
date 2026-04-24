3345391639f34c2b66bed0949ff9fa49fc66d627
Nitish Kumar <nk045729@gmail.com>
2026-04-24 12:12:38 +0530

    update

---
+++ Chapter_7/05_recursion.py
@@ -0,0 +1,13 @@
+'''
+factorial(1) = 1
+factorial(n) = n x n-1 x ........3x2x1
+factorial(n) = n * factorial(n-1)
+'''
+
+def factorial(n):
+    if(n == 1 or n == 0):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("Enter a number : "))
+print(f"The factorial of this number is : {factorial(n)}")
---
+++ Chapter_7/06_practice.py
@@ -0,0 +1,11 @@
+# q1
+
+def greatest(a , b , c):
+    if(a > b and a > c):
+        print("a is greatest")
+    elif(b > a and b > c):
+        print("b is greatest")
+    else:
+        print("c is greatest")
+        
+greatest(1,2,3)
